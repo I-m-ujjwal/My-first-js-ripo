6a42e7f0218f665a0511064af1039a8cd44aedf5
I-m-ujjwal <um930223@gmail.com>
2026-04-22 15:23:19 +0000

    Learned Data types in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//for learning JS use tc39 and MDN
+
+//------Data types ------
+
+// number
+// BigInt
+// string => ""
+// boolean => T/F
+// null => standalone value
+// undefined
+// Symbol
+
+// object
+
+const studentRollno = 123456
+let studentName ="Ujjwal Mishra"
+var studentEmail ="dcgt@gmail.com"//prefer not to use var because of issue in block scope
+
+let studentBranch;
+let studentCity = "Chhatarpur"
+
+
+console.log(typeof studentBranch,typeof studentCity,typeof studentEmail,typeof studentName, typeof studentRollno)
+
