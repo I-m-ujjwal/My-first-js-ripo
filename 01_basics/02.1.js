67ecea6822a355db76fb0c4c853dd44df0ba9945
I-m-ujjwal <um930223@gmail.com>
2026-04-25 12:03:54 +0000

    learned summary of datatypes

---
+++ 01_basics/02.1.js
@@ -0,0 +1,30 @@
+/******   primitive datatype  ******/
+
+//  7 types : string, Number, Boolean, Null, undefined, symbol, Bigint
+
+
+/*****  non-primitive  (reference)  *****/
+
+//  array, objects, functions
+
+/*** there are two types of language 
+
+  1. statically type : A statically typed language is one where the data type of a variable is fixed at compile time.
+   Ex : int a = 10;
+a = 20;       ✅ valid
+a = "hello";  ❌ error
+
+
+ 2. Dynamically type: A dynamically typed language is one where the data type is decided at runtime.
+ Ex : let a = 10;      // number
+a = "hello";     // string ✅ allowed
+a = true;        // boolean ✅ allowed
+
+*/
+
+//***********JS IS A DYNAMICALLY TYPED LANGUAGE ******/
+
+
+//----------------------------------------------
+
+//  stack (primitive), Heap(Non-primitive)
